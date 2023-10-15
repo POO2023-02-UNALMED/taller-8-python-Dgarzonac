eef68a5fab92c9a71649f48a32a13d6023cb66d4
Dgarzonac <123409139+Dgarzonac@users.noreply.github.com>
2023-10-15 01:29:17 -0500

    Update futbolista.py

--- futbolista.py
+++ futbolista.py
@@ -26,5 +26,5 @@ class Futbolista(Persona, Deportista):
         self._listaFutbolistas = listaFutbolistas
     
     def __str__(self):
-        return f"Mi nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} Participando años en el deporte"
+        return f"Mi nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte"
     
